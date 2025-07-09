0961c15fe6fb2bff7ccdd798ef1b51fdbc1d3a9b
David <david7delacruz@gmail.com>
2025-07-09 06:03:02 +0000

    Great! This is the icon for Mustard: <?xml version="1.0" encoding="UTF-8

--- src/components/icons/mustard-icon.tsx
+++ src/components/icons/mustard-icon.tsx
@@ -2,22 +2,18 @@ import type { LucideProps } from "lucide-react";
 import * as React from "react";
 
 const MustardIcon = (props: LucideProps) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    {...props}
-  >
-    <path d="M6 14h12v6H6z"/>
-    <path d="M8 14V8c0-1.1.9-2 2-2h4c1.1 0 2 .9 2 2v6"/>
-    <path d="M8 4h8"/>
-  </svg>
+    <svg
+        id="Layer_1"
+        data-name="Layer 1"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 113.39 113.39"
+        fill="currentColor"
+        {...props}
+    >
+        <path d="M68.53,41.8h-23.68c-4,0-7.25,3.53-7.25,7.88v40.66c0,4.35,3.24,7.88,7.25,7.88h23.68c4,0,7.25-3.53,7.25-7.88v-40.66c0-4.35-3.24-7.88-7.25-7.88ZM72.58,90.35c0,2.58-1.82,4.68-4.05,4.68h-23.68c-2.23,0-4.04-2.1-4.04-4.68v-40.66c0-2.58,1.81-4.68,4.04-4.68h23.68c2.23,0,4.05,2.1,4.05,4.68v40.66Z"/>
+        <path d="M69.84,32.73c0-1-.81-1.81-1.81-1.81h-22.67c-1,0-1.81.81-1.81,1.81v6.47h26.29v-6.47ZM66.64,36h-19.89v-1.88h19.89v1.88Z"/>
+        <polygon points="58.45 15.15 54.93 15.15 53.38 28.83 60.01 28.83 58.45 15.15"/>
+    </svg>
 );
 
 export default MustardIcon;
